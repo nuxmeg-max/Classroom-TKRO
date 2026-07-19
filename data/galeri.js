085ed8a379947c6cd1f06d6c6d2fd2936b24dc39
nuxmeg-max <nuxmeg@gmail.com>
2026-07-19 20:40:48 +0700

    Add gallery data for images and captions

---
+++ data/galeri.js
@@ -0,0 +1,6 @@
+export const galeri = [
+  { src: "/gallery/praktik-mesin-1.jpg", caption: "Praktik overhaul mesin" },
+  { src: "/gallery/praktik-kelistrikan-1.jpg", caption: "Praktik kelistrikan body" },
+  { src: "/gallery/kelas-1.jpg", caption: "Foto bersama kelas" },
+  { src: "/gallery/bengkel-1.jpg", caption: "Suasana bengkel praktik" },
+];
