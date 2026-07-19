fba8d716491b203c0ba1df7b1d1d9f23185ad0fa
nuxmeg-max <nuxmeg@gmail.com>
2026-07-19 20:39:58 +0700

    Add weekly schedule data for classes

---
+++ data/jadwal.js
@@ -0,0 +1,28 @@
+export const jadwal = {
+  Senin: [
+    { jam: "1-2", waktu: "07:00-08:20", mapel: "Upacara / PKn", guru: "Bpk. Slamet" },
+    { jam: "3-4", waktu: "08:20-09:40", mapel: "Matematika", guru: "Ibu Rahma" },
+    { jam: "5-8", waktu: "10:00-13:00", mapel: "Praktik: Pemeliharaan Mesin Kendaraan Ringan", guru: "Bpk. Joko", praktik: true },
+  ],
+  Selasa: [
+    { jam: "1-2", waktu: "07:00-08:20", mapel: "Bahasa Indonesia", guru: "Ibu Sari" },
+    { jam: "3-4", waktu: "08:20-09:40", mapel: "Gambar Teknik Otomotif", guru: "Bpk. Hendra" },
+    { jam: "5-8", waktu: "10:00-13:00", mapel: "Praktik: Sasis dan Pemindah Tenaga", guru: "Bpk. Wawan", praktik: true },
+  ],
+  Rabu: [
+    { jam: "1-2", waktu: "07:00-08:20", mapel: "Bahasa Inggris", guru: "Ibu Dian" },
+    { jam: "3-4", waktu: "08:20-09:40", mapel: "IPAS", guru: "Bpk. Rudi" },
+    { jam: "5-8", waktu: "10:00-13:00", mapel: "Praktik: Kelistrikan Kendaraan Ringan", guru: "Bpk. Anton", praktik: true },
+  ],
+  Kamis: [
+    { jam: "1-2", waktu: "07:00-08:20", mapel: "PAI", guru: "Bpk. Yusuf" },
+    { jam: "3-4", waktu: "08:20-09:40", mapel: "Simulasi & Komunikasi Digital", guru: "Ibu Nita" },
+    { jam: "5-6", waktu: "10:00-11:20", mapel: "P5 / Proyek Kreatif Kewirausahaan", guru: "Bpk. Slamet" },
+    { jam: "7-8", waktu: "11:20-13:00", mapel: "Pekerjaan Dasar Teknik Otomotif", guru: "Bpk. Joko", praktik: true },
+  ],
+  Jumat: [
+    { jam: "1-2", waktu: "07:00-08:20", mapel: "PJOK", guru: "Bpk. Doni" },
+    { jam: "3-4", waktu: "08:20-09:40", mapel: "Kimia", guru: "Ibu Rahma" },
+    { jam: "5-6", waktu: "10:00-11:20", mapel: "Praktik Bengkel (Umum)", guru: "Bpk. Wawan", praktik: true },
+  ],
+};
