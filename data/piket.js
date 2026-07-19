c5a7df93938127137cce220e20cabe2c05cfd741
nuxmeg-max <nuxmeg@gmail.com>
2026-07-19 20:40:23 +0700

    Add piket.js with weekly schedule data

---
+++ data/piket.js
@@ -0,0 +1,7 @@
+export const piket = {
+  Senin: ["Ahmad Fauzi", "Bayu Setiawan", "Candra Wibowo", "Doni Firmansyah"],
+  Selasa: ["Eko Purnomo", "Farhan Hidayat", "Galih Pratama", "Hendra Gunawan"],
+  Rabu: ["Irfan Maulidi", "Joko Susilo", "Krisna Aditya", "Lutfi Ramadhan"],
+  Kamis: ["Muhammad Rizal", "Nanda Saputra", "Oscar Firmansyah", "Panji Anggara"],
+  Jumat: ["Rafi Ardiansyah", "Satria Nugraha", "Taufik Hidayat", "Umar Faruq", "Wahyu Ilahi"],
+};
